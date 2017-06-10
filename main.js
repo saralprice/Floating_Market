a1a7cef122d681993a3e427e7bc410483935d447
saralprice <saralouise.price@gmail.com>
2017-06-10 21:08:46 +0100

    Experimenting with basic javascript functions

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-var trade = 0;
+var trade = 0,
+pom = 3;
+
+/*
+or could also do as this...
+	var trade, pom;
+	trade = 0;
+	pom = 3;
+*/
+
+//good practice to put spaces around math function bits like = or *
+
+document.getElementById("pom").innerHTML = pom;
+// learned how to define a bit of html with matching id to variable value I've set
 
 function tradeClick(number){
 	trade = trade + number;
 	document.getElementById("trade").innerHTML = trade;
 	};
+//don't understand this bit properly	
